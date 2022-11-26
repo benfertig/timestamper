7c9fcff85c9fc2c92b3d50dea5c4d21304b79d0c
Ben Fertig <50497938+benfertig@users.noreply.github.com>
2022-11-26 15:03:32 -0500

    Update mac_setup.py

--- src/mac_setup.py
+++ src/mac_setup.py
@@ -8,60 +8,32 @@ Usage:
 from glob import glob
 from setuptools import setup
 
-# The "classes" directory
-classes_dir =  "classes"
-
-# The "macros" directory
-macros_dir = f"{classes_dir}/macros"
-
-# The "template" directory
-template_dir = f"{classes_dir}/template"
-
-# The template subdirectories
-buttons_dir = f"{classes_dir}/template/buttons"
-entries_dir = f"{classes_dir}/template/entries"
-labels_dir = f"{classes_dir}/template/labels"
-texts_dir = f"{classes_dir}/template/texts"
-windows_dir = f"{classes_dir}/template/windows"
-
-# The "timing" directory
-timing_dir = f"{classes_dir}/timing"
-
-# The "widgets" directory
-widgets_dir = f"{classes_dir}/widgets"
-
-# The "images" directory
-images_dir = "images"
-
-# The "messages" directory
-messages_dir = "messages"
-
 APP = ["Time Stamper.py"]
 
 DATA_FILES = [
 
-    # Add the macros to the data files.
-    (macros_dir, glob(f"{macros_dir}/*.*")),
+    # Add the macros.
+    ("classes/macros", glob("classes/macros/*.*")),
 
-    # Add the templates to the data files.
-    (template_dir, glob(f"{template_dir}/*.*")),
-    (buttons_dir, glob(f"{buttons_dir}/*.*")),
-    (entries_dir, glob(f"{entries_dir}/*.*")),
-    (labels_dir, glob(f"{labels_dir}/*.*")),
-    (texts_dir, glob(f"{texts_dir}/*.*")),
-    (windows_dir, glob(f"{windows_dir}/*.*")),
+    # Add the templates.
+    ("classes/template", glob("classes/template/*.*")),
+    ("classes/template/buttons", glob("classes/template/buttons/*.*")),
+    ("classes/template/entries", glob("classes/template/entries/*.*")),
+    ("classes/template/labels", glob("classes/template/labels/*.*")),
+    ("classes/template/texts", glob("classes/template/texts/*.*")),
+    ("classes/template/windows", glob("classes/template/windows/*.*")),
 
-    # Add the timing files to the data files.
-    (timing_dir, glob(f"{timing_dir}/*.*")),
+    # Add the timing files.
+    ("classes/timing", glob("classes/timing/*.*")),
 
-    # Add the widget files. to the data files.
-    (widgets_dir, glob(f"{widgets_dir}/*.*")),
+    # Add the widget files.
+    ("classes/widgets", glob("classes/widgets/*.*")),
 
-    # Add the images to the data files.
-    (images_dir, glob(f"{images_dir}/*.*")),
+    # Add the images.
+    ("images", glob("images/*.*")),
 
-    # Add the messages to the data files.
-    (messages_dir, glob(f"{messages_dir}/*.*"))
+    # Add the messages.
+    ("messages", glob("messages/*.*"))
 ]
 
 OPTIONS = {"includes": ["tkmacosx"],
